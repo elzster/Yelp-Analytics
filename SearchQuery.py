e334bc0dcb1404431083921b5b67b14c0a454803
elzster <hakim.elie1@gmail.com>
2019-10-01 16:21:42 -0400

    Updated Files

--- SearchQuery.py
+++ SearchQuery.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import requests
 from config import api_key
-
+print("Welcome to our Yelp Search Program!!!\nHere you can search many categories by city and state,\nAnd export the listings to a near CSV file.\nJust follow the Prompts!\n")
 print("What type of Category would you like to search for? i.e Restaurant")
 thing = input()
 print("What city and state are you searching? i.e. New York City, NY")
@@ -22,12 +22,12 @@ review_count = []
 business_id = []
 rating = []
 
-def query_search(set_num): 
+def query_search(set_num):
     url = 'https://api.yelp.com/v3/businesses/search'
     headers = {
         'Authorization': 'Bearer {}'.format(api_key),}
     url_params = { #parameters passed to the API
-    "categories": thing,
+    "term": thing,
     "location": location,
     'offset': offset_num,
      "limit":50
@@ -36,10 +36,10 @@ def query_search(set_num):
     response = requests.get(url, headers=headers, params=url_params)
     return response.json()
 
-for offset_num in range(offset): 
+for offset_num in range(offset):
     try:
         response = query_search(offset_num)
-        businesses=response['businesses']            
+        businesses=response['businesses']
         for x in businesses:
             name.append(x['name'])
             business_id.append(x['id'])
@@ -59,7 +59,7 @@ for offset_num in range(offset):
         print("error at ", offset_num)
     except KeyError:
         print("error at ", offset_num)
-        
+
 business_df = pd.DataFrame({
     "Business" : name,
     "Business ID": business_id,
@@ -74,4 +74,4 @@ business_df = pd.DataFrame({
 })
 business_df
 
-business_df.to_csv('data/export.csv')
+business_df.to_csv('data/'+(location)+'_'+(thing)+'.csv')
